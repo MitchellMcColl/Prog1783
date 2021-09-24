af9bec7292e85794589f106133cb54ca96e3b86b
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-09-24 09:32:55 -0400

    removed h  in eligible

--- hands on work.py
+++ hands on work.py
@@ -7,4 +7,4 @@ if age < 18:
     print("Sorry... you are unable to vote... come back soon ya hear!")
 
 else:
-    print("You are heligible to vote - proceed with your civic duty citizen!")
+    print("You are eligible to vote - proceed with your civic duty citizen!")
