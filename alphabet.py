da65a258fd91de6b17e546068b073df3adeee521
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-09-24 09:56:01 -0400

    :

--- alphabet.py
+++ alphabet.py
@@ -1,4 +1,9 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-print(alphabet[0])
+print(alphabet[0])  
 print(alphabet[15])
 print(alphabet[23])
+
+print(alphabet[:])
+print(alphabet[:5])
+print(alphabet[6:17])
+print(alphabet[:23])
