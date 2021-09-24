3dbc0a8501f0bcf30296948d610caf7b002ca26a
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-09-24 09:16:37 -0400

    hands on work

---
+++ hands on work.py
@@ -0,0 +1,10 @@
+age = 0
+print("Enter your age here! ")
+age = int(input())
+
+
+if age < 18:
+    print("Sorry... you are unable to vote... come back soon ya hear!")
+
+else:
+    print("You are eligible to vote - proceed with your civic duty citizen!")
