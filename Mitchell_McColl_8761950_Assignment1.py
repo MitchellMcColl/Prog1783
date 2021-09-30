da58a412179d86504a93421d03d36e30d0959cb0
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-09-30 14:15:50 -0400

    final code

--- Mitchell_McColl_8761950_Assignment1.py
+++ Mitchell_McColl_8761950_Assignment1.py
@@ -1,5 +1,24 @@
+price = 0.0
+hst = 0.0
+amount = 0
+final = 0.0
+shirt = 0
+
 color = input("Please input what color of shirt you want: ")
 shirt = input("Please enter 1 for polo or 2 for t-shirt: ")
+
+if int(shirt) == 1:
+    type = "polo shirts"
+elif int(shirt) == 2:
+    type = "t-shirts"   
+else:
+    print("error")
+    exit()
+    
+
+
 amount = input("Please enter the amount of shirts you want: ")
-
-
+price = float(amount) * 9.99
+hst = price * 0.13
+final = hst + price
+print("you bought " + amount + " " + color + " " + type + " for a total price of $" + str(final))
