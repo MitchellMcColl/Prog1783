86d18752f1e3d05886aed5a48839990bbe9425e5
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-09-30 13:19:40 -0400

    inputs

---
+++ Mitchell_McColl_8761950_Assignment1.py
@@ -0,0 +1,5 @@
+color = input("Please input what color of shirt you want: ")
+shirt = input("Please enter 1 for polo or 2 for t-shirt: ")
+amount = input("Please enter the amount of shirts you want: ")
+
+
