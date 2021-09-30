37a6878d93d78f1766ab26f703266ad927a60da1
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-09-30 14:41:22 -0400

    welcome message

--- Mitchell_McColl_8761950_Assignment1.py
+++ Mitchell_McColl_8761950_Assignment1.py
@@ -3,6 +3,7 @@ hst = 0.0
 amount = 0
 final = 0.0
 shirt = 0
+print("This program will ask the user what color they want, what type of shirt they want, and the amount of shirts. The program will then calculate the amount of shirts with the price then calculate and add the HST to the final price\n\n")
 
 color = input("Please input what color of shirt you want: ")
 shirt = input("Please enter 1 for polo or 2 for t-shirt: ")
@@ -15,7 +16,6 @@ else:
     print("error")
     exit()
     
-
 
 amount = input("Please enter the amount of shirts you want: ")
 price = float(amount) * 9.99
