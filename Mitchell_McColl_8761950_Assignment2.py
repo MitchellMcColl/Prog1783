a0bce7ff73f847399d2e6e2b7b6ce34e49ce51a4
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-10-21 21:16:34 -0400

    commit2

--- Mitchell_McColl_8761950_Assignment2.py
+++ Mitchell_McColl_8761950_Assignment2.py
@@ -5,30 +5,55 @@ amountP = 0
 final = 0.0
 shirt = 0
 pants = 0
-colorS = 0
-print("This program will ask the user what color they want, what type of shirt they want, and the amount of shirts. The program will then calculate the amount of shirts with the price then calculate and add the HST to the final price\n\n")
+colorP = 0
+colorint = 0
 
 color = input("Please input what color of shirt you want: ")
-shirt = input("Please enter 1 for polo or 2 for t-shirt: ")
+while True:
+    shirt = int(input("Please enter 1 for polo or 2 for t-shirt: "))
 
-while colorS != 1 or colorS != 2 or colorS != 3:
-    colorS = int(input("Please enter the color of pants you want: 1 for Blue, 2 for Black, 3 for Red: "))
-    if colorS != 1 or colorS != 2 or colorS != 3:
-        print("Please choose either 1, 2, or 3")
-
-pants = input("Please enter 1 for pants or 2 for shorts: ")
-
-if int(shirt) == 1:
-    type = "polo shirts"
-elif int(shirt) == 2:
-    type = "t-shirts"   
-else:
-    print("error")
-    exit()
-    
+    if int(shirt) == 1:
+        type = "polo shirts"
+        break
+    elif int(shirt) == 2:
+        type = "t-shirts" 
+        break
+    else:
+        print("Please enter either 1 or 2")  
 
 amountS = input("Please enter the amount of shirts you want? Each shirt is $9.99: ")
-price = float(amount) * 9.99
+
+
+while True:
+    pants = int(input("Please enter 1 for pants or 2 for shorts: "))
+
+    if int(pants) == 1:
+        typeP = "pants"
+        break
+    elif int(pants) == 2:
+        typeP = "shorts"
+        break
+    else:
+        print("Please enter either 1 or 2")  
+
+while True:
+    colorint = input("Please enter the color of " + typeP + " you want. 1 for Blue, 2 for Red, or 3 for Black")
+    if int(colorint) == 1:
+        colorP = "blue"
+        break
+    elif int(colorint) == 2:
+        colorP = "red"
+        break
+    elif int(colorint) == 3:
+        colorP = "black"
+        break
+    else:
+        print("Please enter either 1, 2, or 3")  
+
+
+
+
+price = float(amountS) * 9.99
 hst = price * 0.13
 final = hst + price
 print("you bought " + amountS + " " + color + " " + type + " for a total price of $" + str(final))
