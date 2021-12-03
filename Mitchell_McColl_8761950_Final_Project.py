de27d0626407937016b9362ad8decbc2b2e7de6b
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-12-03 14:45:31 -0500

    choose method finished

--- Mitchell_McColl_8761950_Final_Project.py
+++ Mitchell_McColl_8761950_Final_Project.py
@@ -1,15 +1,25 @@
 import random
 
 class RockPaperScissors:
-    def __init__(self, player,computer, computerChoices):
+    def __init__(self, player,computer, computerChoices, playerpoints, computerpoints):
         self.player = player
         self.computer = computer
         self.computer_choices = computerChoices
+        self.playerpoints = playerpoints
+        self.computerppoints = computerpoints
     
     
     def choose(self):
         global player, computer, computerChoices
-        
+       
+        computer = random.choice(computerChoices)
+
+        while True:
+            player = input("Please enter your choice of either, 'rock, paper, scissors': ")
+            if player == "rock" or player == "Rock" or player == "paper" or player == "Paper" or player == "scissors" or player == "Scissors":
+                rps.calculateWinner()
+            else:
+                print(player + " is not a correct choice.") 
     
     def calculateWinner(self):
         return
@@ -19,6 +29,14 @@ class RockPaperScissors:
 
 computerChoices = ["Rock", "Paper", "Scissors"]
 player = ""
+playerpoints = 0
 computer = ""
-rps = RockPaperScissors(player, computer, computerChoices)
+computerpoints = 0
+
+rps = RockPaperScissors(player, computer, computerChoices, playerpoints, computerpoints)
+
+while playerpoints or computerpoints != 3:
+    rps.choose()
+
+
 
