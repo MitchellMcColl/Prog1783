b547945b7e6e458b37e73e5f134f2b01593b6c63
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-12-03 09:23:08 -0500

    final changes q3

--- Mitchell_McColl_Test3_Q3.py
+++ Mitchell_McColl_Test3_Q3.py
@@ -4,7 +4,8 @@ sum = 0
 #removed unessesary m variable
 #removed the ,2 from the range
 for num in range(1, n + 1):
-    sum = sum + num
+#changed sum = sum + num to sum +=num
+    sum += num
 print("The sum of the first", n, "numbers is: ", sum)
 #changed: sum / (n + 1) to just: sum / n
 average = sum / n
