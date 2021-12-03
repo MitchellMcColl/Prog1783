c52cb37b54ffe28d7c3c7fed00e19b4c97b905f6
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-12-03 14:06:29 -0500

    added methods

---
+++ Mitchell_McColl_8761950_Final_Project.py
@@ -0,0 +1,24 @@
+import random
+
+class RockPaperScissors:
+    def __init__(self, player,computer, computerChoices):
+        self.player = player
+        self.computer = computer
+        self.computer_choices = computerChoices
+    
+    
+    def choose(self):
+        global player, computer, computerChoices
+        
+    
+    def calculateWinner(self):
+        return
+    
+    def displayResults(self):
+        return
+
+computerChoices = ["Rock", "Paper", "Scissors"]
+player = ""
+computer = ""
+rps = RockPaperScissors(player, computer, computerChoices)
+
