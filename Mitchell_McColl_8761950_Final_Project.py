a84c9e2336f92e4a5e3bc23ae138e7b2e59c0e11
Mitchell McColl <mmccoll1950@conestogac.on.ca>
2021-12-03 16:11:30 -0500

    added a playagain feature

--- Mitchell_McColl_8761950_Final_Project.py
+++ Mitchell_McColl_8761950_Final_Project.py
@@ -1,12 +1,13 @@
 import random
 
 class RockPaperScissors:
-    def __init__(self, player, computer, computerChoices, playerpoints, computerpoints):
+    def __init__(self, player, computer, computerChoices, playerpoints, computerpoints, playagain):
         self.player = player
         self.computer = computer
         self.computer_choices = computerChoices
         self.playerpoints = playerpoints
         self.computerppoints = computerpoints
+        self.playagain = playagain
     
     
     def choose(self):
@@ -39,7 +40,7 @@ class RockPaperScissors:
                 print("You lose! " + computer + " beats " + player)
                 computerpoints += 1
             else:
-                print("You win! " + player + " beats" + computer)
+                print("You win! " + player + " beats " + computer)
                 playerpoints += 1
 
         elif player == "paper":
@@ -62,13 +63,39 @@ class RockPaperScissors:
         
     
     def displayGameWinner(self):
+        global playagain, playerpoints, computerpoints
         if computerpoints == 3:
             print("Computer has gotten 3 points! Computer wins")
-            exit()
+            playagain = input("Please enter 'Yes' to play again or 'No' to stop: ")
+            playagain = playagain.lower()
+            if playagain == "yes" or playagain == "y":
+                computerpoints = 0
+                playerpoints = 0
+                return
+            
+            elif playagain == "no" or playagain == "n":
+                exit()
+            
+            else:
+                print("Please enter either yes or no")
+
+            
         
         elif playerpoints == 3:
             print("Player has gotten 3 points! Player wins")
-            exit()
+            playagain = input("Please enter 'Yes' to play again or 'No' to stop: ")
+            playagain = playagain.lower()
+            if playagain == "yes" or playagain == "y":
+                computerpoints = 0
+                playerpoints = 0
+                return
+            
+            elif playagain == "no" or playagain == "n":
+                exit()
+            
+            else:
+                print("Please enter either yes or no")
+        
         print("The Player has: " + str(playerpoints))
         print("The Computer has: " + str(computerpoints))
         print("First to 3 points wins\n\n")
@@ -79,8 +106,9 @@ player = ""
 playerpoints = 0
 computer = ""
 computerpoints = 0
+playagain = ""
 
-rps = RockPaperScissors(player, computer, computerChoices, playerpoints, computerpoints)
+rps = RockPaperScissors(player, computer, computerChoices, playerpoints, computerpoints, playagain)
 
 rps.displayGameWinner()
 while playerpoints or computerpoints != 3:
